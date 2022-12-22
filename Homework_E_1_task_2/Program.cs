15fd124be6d325fb482ce25defac200b8743d294
Shoo <lumen.gaming777@gmail.com>
2022-12-22 19:42:43 +0200

    Finished task 2 HW 1

--- Homework_E_1_task_2/Program.cs
+++ Homework_E_1_task_2/Program.cs
@@ -6,7 +6,28 @@ namespace Homework_E_1_task_2
     {
         static void Main(string[] args)
         {
-            
+            Book book = new Book(ReadBookTitle(), ReadBookContent(), ReadBookAuthor());
+        }
+
+        private static string ReadBookTitle()
+        {
+            Console.WriteLine("Please enter book title: ");
+            string bookTitle = Console.ReadLine();
+            return bookTitle;
+        }
+
+        private static string ReadBookContent()
+        {
+            Console.WriteLine("Please enter book content: ");
+            string bookContent = Console.ReadLine();
+            return bookContent;
+        }
+
+        private static string ReadBookAuthor()
+        {
+            Console.WriteLine("Please enter book author: ");
+            string bookAuthor = Console.ReadLine();
+            return bookAuthor;
         }
     }
 }
