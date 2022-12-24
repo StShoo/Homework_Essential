e3bf2c7fec773340f364ba3e765ee18c5e83a5f5
Shoo <lumen.gaming777@gmail.com>
2022-12-24 13:29:10 +0200

    Created homework 2 project, started doing first task

---
+++ Homework_E_2_task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_E_2_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
