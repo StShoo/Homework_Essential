845d9861c472e15a23930c08825fde3fd88d0892
Shoo <lumen.gaming777@gmail.com>
2022-12-24 21:58:13 +0200

    Finished task 4

--- Homework_E_2_task_4/Program.cs
+++ Homework_E_2_task_4/Program.cs
@@ -6,7 +6,41 @@ namespace Homework_E_2_task_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter login: ");
+            string account = Console.ReadLine();
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter surname: ");
+            string surname = Console.ReadLine();
+            Console.Write("Enter youre age: ");
+            int age = ReadIntValue();
+            Console.Write("Enter date of registration: ");
+            string dateOfRegistration = Console.ReadLine();
+
+            User userData = new User(account, name, surname, age, dateOfRegistration);
+
+            Console.WriteLine("User Data:\n" +
+                $"Login: {userData.Login}\n" +
+                $"Name: {userData.Name}\n" +
+                $"Surname: {userData.Surname}\n" +
+                $"Age: {userData.Age}\n" +
+                $"Date of registration: {userData.DateOfRegistration}");
+        }
+
+            private static int ReadIntValue()
+        {
+            do
+            {
+                try
+                {
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    return number;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter only int values");
+                }
+            } while (true);
         }
     }
 }
