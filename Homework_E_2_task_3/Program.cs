b1a3f5e04676edd9442e1b7752f8dbe3283b1fe6
Shoo <lumen.gaming777@gmail.com>
2022-12-24 20:43:26 +0200

    Finished task 3

--- Homework_E_2_task_3/Program.cs
+++ Homework_E_2_task_3/Program.cs
@@ -6,7 +6,63 @@ namespace Homework_E_2_task_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter account: ");
+            int account = ReadIntValue();
+            Console.Write("Enter custumer: ");
+            string custumer = Console.ReadLine();
+            Console.Write("Enter provider: ");
+            string provider = Console.ReadLine();
+            Console.Write("Enter product name: ");
+            string article = Console.ReadLine();
+            Console.Write("Enter quantity: ");
+            int quantity = ReadIntValue();
+            Console.Write("Enter VAT rate in youre country: ");
+            double vatRate = ReadDoubleValue();
+
+            Invoice invoice = new Invoice(account, custumer, provider)
+            {
+                Article = article,
+                Quantity = quantity,
+                VatRate = vatRate
+            };
+
+            invoice.CalculateCost();
+
+            Console.WriteLine($"Cost without VAT is: {invoice.CostWithoutVAT}\n" +
+                $"Cost including VAT is: {invoice.CostWithVAT}");
         }
+
+        private static int ReadIntValue()
+        {
+            do
+            {
+                try
+                {
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    return number;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter only int values");
+                }
+            } while (true);
+        }
+
+        private static double ReadDoubleValue()
+        {
+            do
+            {
+                try
+                {
+                    double number = Convert.ToDouble(Console.ReadLine());
+                    return number;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter only double values");
+                }
+            } while (true);
+        }
+
     }
 }
