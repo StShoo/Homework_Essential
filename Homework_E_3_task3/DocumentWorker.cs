5879e40cbf236258f377100385c414e5260a8c29
Shoo <lumen.gaming777@gmail.com>
2022-12-25 18:14:42 +0200

    Finished task 3

---
+++ Homework_E_3_task3/DocumentWorker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_E_3_task3
+{
+    class DocumentWorker
+    {
+        public virtual void OpenDocument()
+        {
+            Console.WriteLine("Документ відкритий");
+        }
+
+        public virtual void EditDocument()
+        {
+            Console.WriteLine("Редагування документа у версії Про");
+        }
+
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("Збереження документа у версії Про");
+        }
+    }
+
+    class ProDocumentWorker: DocumentWorker
+    {
+        public override void EditDocument()
+        {
+            Console.WriteLine("Документ відредаговано");
+        }
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ збережено у старому форматі, збереження в інших форматах є у версії Експерт");
+        }
+    }
+
+    class ExpertDocumentWorker: DocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ збережений у новому форматі");
+        }
+    }
+
+}
---
+++ Homework_E_3_task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Homework_E_3_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter key to activate Pro or Expert version (if you don`t have key enter \"skip\"): ");
+            string key = CheckKey();
+
+            if (key.Equals("Pro"))
+            {
+                DocumentWorker proWorker = new ProDocumentWorker();
+                proWorker.OpenDocument();
+                proWorker.EditDocument();
+                proWorker.SaveDocument();
+            } 
+            else if (key.Equals("Expert"))
+            {
+                DocumentWorker expertWorker = new ExpertDocumentWorker();
+                expertWorker.OpenDocument();
+                expertWorker.EditDocument();
+                expertWorker.SaveDocument();
+            }
+            else
+            {
+                DocumentWorker docWorker = new DocumentWorker();
+                docWorker.OpenDocument();
+                docWorker.EditDocument();
+                docWorker.SaveDocument();
+            }
+            
+        }
+
+        private static string CheckKey(){ return Console.ReadLine(); }
+    }
+}
