004c182acaa3809135f68658dd19707f0f60a0fd
Shoo <lumen.gaming777@gmail.com>
2022-12-25 15:14:34 +0200

    Created solution and first task project

---
+++ Homework_E_3_task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_E_3_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
