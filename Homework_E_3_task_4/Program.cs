a172bc6b7a893114fe2e7f7776f8e54fc74a1ffd
Shoo <lumen.gaming777@gmail.com>
2022-12-25 18:17:08 +0200

    Added task 4 empty project

---
+++ Homework_E_3_task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_E_3_task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
