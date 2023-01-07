cf879ed96dd093599a652e96852340f0ac28b18f
lumen.gaming777@gmail.com <lumen.gaming777@gmail.com>
2023-01-07 20:16:21 +0200

    Finished task 2

---
+++ Homework_E_5_task_2/MyMatrix.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_E_5_task_2
+{
+    internal class MyMatrix
+    {
+        int len;
+        int wid;
+
+        public MyMatrix(int len, int wid)
+        {
+            this.len = len;
+            this.wid = wid;
+        }
+
+        private int[,] CrateMatrix()
+        {
+            int[,] matrix = new int[len, wid];
+            return matrix;
+        }
+
+        private int[,] FillMatrix(int[,] matrix)
+        {
+            Random random= new Random();
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(0, 10);
+                }
+            }
+            return matrix;
+        }
+
+        public void PrintMatrix()
+        {
+            int[,] matrix = FillMatrix(CrateMatrix());
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Homework_E_5_task_2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Homework_E_5_task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter matrix size: ");
+            MyMatrix firstMatrix = new MyMatrix(ReadValue(), ReadValue());
+
+            Console.WriteLine("Youre matrix: ");
+            firstMatrix.PrintMatrix();
+
+            Console.WriteLine("Do you want to change it? (Y/N)");
+            string answer = Console.ReadLine();
+
+            if (answer.Equals("Y"))
+            {
+                MyMatrix secondMatrix = new MyMatrix(ReadValue(), ReadValue());
+                Console.WriteLine("Youre final matrix is: ");
+                secondMatrix.PrintMatrix();
+            }
+        }
+
+        static int ReadValue()
+        {
+            do
+            {
+                try
+                {
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    return number;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter only int values");
+                }
+            } while (true);
+        }
+    }
+}
