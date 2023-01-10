e16fb804afc31241b0761098e199a6a5e2f0f916
lumen.gaming777@gmail.com <lumen.gaming777@gmail.com>
2023-01-10 16:24:31 +0200

    Finished task 1

---
+++ Homework_E_6_task_1/FindAndReplaceManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_E_6_task_1
+{
+    static class FindAndReplaceManager
+    {
+        public static void FindNext(string str)
+        {
+            Console.WriteLine("Trying to find youre string.\n" +
+                "Sorry, surch was unseccessful");
+        }
+    }
+}
---
+++ Homework_E_6_task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_E_6_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindAndReplaceManager.FindNext("My string");
+        }
+    }
+}
