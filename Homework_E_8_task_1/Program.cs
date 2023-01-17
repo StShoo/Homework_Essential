e20852fc1fd4e84e7c790f6c6e1413a78c4489f0
lumen.gaming777@gmail.com <lumen.gaming777@gmail.com>
2023-01-17 14:32:13 +0200

    Created first task lesson 8

---
+++ Homework_E_8_task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework_E_8_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+
+    static class Printer
+    {
+        public static void Print(string stroka, int color)
+        {
+            Console.ForegroundColor = (ConsoleColor)color;
+            Console.WriteLine(stroka);
+        }
+    }
+
+    enum EnumColors : int
+    {
+        Red = 1,
+        Green = 2,
+        Blue = 3,
+        Yellow = 4
+    }
+}
