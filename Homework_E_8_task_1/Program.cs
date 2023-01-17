8f0c5c1dea3cf1d52ca4f989f64d35bb95fec0ac
lumen.gaming777@gmail.com <lumen.gaming777@gmail.com>
2023-01-17 14:34:06 +0200

    Created task 1 lesson 8

--- Homework_E_8_task_1/Program.cs
+++ Homework_E_8_task_1/Program.cs
@@ -4,8 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
-
+            Console.WriteLine("Please enter string you want to print: ");
+            string textToPrint = Console.ReadLine();
+            Console.WriteLine("Please enter colore: \n" +
+                "Red = 1,\r\n" +
+                "Green = 2,\r\n" +
+                "Blue = 3,\r\n" +
+                "Yellow = 4");
+            int color = Console.ReadLine();
         }
     }
 
