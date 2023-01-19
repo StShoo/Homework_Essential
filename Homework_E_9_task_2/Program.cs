e00d1f088182d106c3ca230499dab96dd70a07b3
lumen.gaming777@gmail.com <lumen.gaming777@gmail.com>
2023-01-19 20:51:59 +0200

    Started task 2, have an error with converting delegat to double

---
+++ Homework_E_9_task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Homework_E_9_task_2
+{
+
+    public delegate double MidValueDelegat();
+    public delegate int Delegats();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Delegats[] delegatsArr = new Delegats[] {getDelegat(), getDelegat(), getDelegat(), 
+                getDelegat(), getDelegat(), getDelegat(), getDelegat()};
+
+            MidValueDelegat midValueDelegat;
+            double sum = 0;
+
+            midValueDelegat = delegate { for(int i = 0; i < delegatsArr.Length; i++) {double num = delegatsArr[i]; sum += num; };
+                return sum / delegatsArr.Length; };
+        }
+
+        static Delegats getDelegat()
+        {
+            Delegats delegats = new Delegats(RandValue.RandomeValue);
+            return delegats;
+        }
+    }
+
+    static class RandValue
+    {
+        public static int RandomeValue()
+        {
+            Random rnd = new Random();
+            return rnd.Next();
+        }
+    }
+}
