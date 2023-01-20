0f143f55a4ac5d8f6cf5a2365c9a8a0ef80d4b6b
lumen.gaming777@gmail.com <lumen.gaming777@gmail.com>
2023-01-20 19:50:10 +0200

    Created and started task 1

---
+++ Homework_E_10_task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_E_10_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
