7b5f4f9ec7f42090e605035c07294a1a88f55a7d
lumen.gaming777@gmail.com <lumen.gaming777@gmail.com>
2023-01-24 19:59:32 +0200

    finished task 1

--- Homework_E_10_task_1/Program.cs
+++ Homework_E_10_task_1/Program.cs
@@ -1,10 +1,63 @@
 ﻿namespace Homework_E_10_task_1
 {
+    interface IDinamicArray<T>
+    {
+        public void Add(T valueToAdd);
+        public T ValueOf(int index);
+        public int Len();
+    }
+
+    class List<T>: IDinamicArray<T>
+    {
+        T[]? mylist;
+        readonly int listLength;
+        int counterAddedValues = 0;
+
+        public List(int listLength)
+        {
+            this.listLength = listLength;
+            mylist = new T[listLength];
+        }
+
+        public void Add(T valueToAdd)
+        {
+            mylist[counterAddedValues] = valueToAdd;
+            counterAddedValues++;
+        }
+
+        public T ValueOf(int index)
+        {
+            try
+            {
+                return mylist[index];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Sorry could not find value with such index.\nResult is set to defult.");
+                return default;
+            }
+        }
+
+        public int Len()
+        {
+            return mylist.Length;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Please enter list length: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+            List<int> list = new List<int>(len);
+
+            list.Add(12);
+            list.Add(-17);
+            list.Add(2);
+
+            Console.WriteLine(list.Len());
+            Console.WriteLine(list.ValueOf(0));
         }
     }
 }
