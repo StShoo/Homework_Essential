3ff8bae230f331eac0ea0336c70e98ba721bbac7
lumen.gaming777@gmail.com <lumen.gaming777@gmail.com>
2023-01-26 17:21:38 +0200

    Finished task 5

---
+++ Homework_E_11_task_5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework_E_11_task_5
+{
+    class MyClass<T> where T: new()
+    {
+        T instance = new T();
+        public T FacrotyMethod()
+        {
+            return instance;
+        }
+    }
+    class NewClass{ }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<NewClass> inst= new MyClass<NewClass>();
+        }
+    }
+}
