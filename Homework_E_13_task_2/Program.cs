5360b85dbec6fd7f7dcc01c2ccc334627b1c6493
lumen.gaming777@gmail.com <lumen.gaming777@gmail.com>
2023-02-02 16:18:06 +0200

    Have trobles doing 1 and 2 tasks, will come back to theme later

--- Homework_E_13_task_2/Program.cs
+++ Homework_E_13_task_2/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Homework_E_13_task_2
+﻿/*namespace Homework_E_13_task_2
 {
     class Program
     {
@@ -96,4 +90,4 @@ namespace Homework_E_13_task_2
             }
         }
     }
-}
+}*/
