99fa470d3a6568b4205a056631cfad7b95d6639c
lumen.gaming777@gmail.com <lumen.gaming777@gmail.com>
2023-02-09 21:57:04 +0200

    Finished task 3

---
+++ Homework_E_16_task_3/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Homework_E_16_task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date d1 = new Date(23, 12, 2023);
+            Date d2 = new Date(15, 10, 2001);
+
+            Date addDate = d1 + d2;
+            Date subDate = d1 - d2;
+
+            Console.WriteLine($"Added days: Days - {addDate.Day}, Monthes - {addDate.Month}, Years - {addDate.Year}");
+            Console.WriteLine($"Subed days: Days - {subDate.Day}, Monthes - {subDate.Month}, Years - {subDate.Year}");
+        }
+    }
+
+    class Date
+    {
+        int day;
+        int month;
+        int year;
+
+        public Date(int day, int month, int year)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+        public int Day { get { return day; } set {day = value; } }
+        public int Month { get { return month; } set { month = value; } }
+        public int Year { get { return year; } set { year = value; } }
+
+        public static Date operator -(Date d1, Date d2)
+        {
+            return new Date(d1.Day - d2.Day, d1.Month - d2.Month, d1.Year - d2.Year);
+        }
+        public static Date operator +(Date d1, Date d2)
+        {
+            return new Date(d1.Day + d2.Day, d1.Month + d2.Month, d1.Year + d2.Year);
+        }
+    }
+}
